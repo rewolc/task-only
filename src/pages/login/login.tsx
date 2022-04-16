4192350e0f49aeefd682ed4605ceb673ff36ce84
rewolc <erobel0@mail.ru>
2022-04-16 18:22:07 +0300

    small loggin change

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -7,80 +7,74 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 
 import {
-  LoginWraper,
-  LoginButton,
-  LoginCheckbox,
-  LoginLabel,
-  LoginLabelText,
+	LoginWraper,
+	LoginButton,
+	LoginCheckbox,
+	LoginLabel,
+	LoginLabelText,
 } from "./login-styled";
 
 type FormData = {
-  login: string;
-  password: string;
+	login: string;
+	password: string;
 };
 
 const LoginPage = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [login, enteredLogin] = useState("");
-  const [password, enteredPassword] = useState("");
-  const [isBtnActive, changeBtnActive] = useState(true);
+	const [login, enteredLogin] = useState("");
+	const [password, enteredPassword] = useState("");
+	const [isBtnActive, changeBtnActive] = useState(true);
 
-  const sleep = (time: number) =>
-    new Promise((resolve) => setTimeout(resolve, time));
+	const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
-  const forms = ["login", "password"];
+	const forms = ["login", "password"];
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({
-    mode: "onBlur",
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>({
+		mode: "onBlur",
+	});
 
-  const onSubmit = handleSubmit(async (data) => {
-    changeBtnActive(!isBtnActive);
-    await sleep(Math.random() * 3000);
-    enteredLogin(data.login);
-    enteredPassword(data.password);
-    ((isBtnActive) => changeBtnActive(!isBtnActive))();
-  });
+	const onSubmit = handleSubmit(async (data) => {
+		changeBtnActive(!isBtnActive);
+		await sleep(Math.random() * 3000);
+		enteredLogin(data.login);
+		enteredPassword(data.password);
+		((isBtnActive) => changeBtnActive(!isBtnActive))();
+	});
 
-  return (
-    <LoginWraper as="form" onSubmit={onSubmit}>
-      {login ? (
-        login === user.login ? (
-          password === user.password ? (
-            navigate("/entered")
-          ) : (
-            <WarningComponent text={"Неверный пароль"} />
-          )
-        ) : (
-          <WarningComponent text={`Пользователя ${login} не существует`} />
-        )
-      ) : (
-        <></>
-      )}
-      {forms.map((form, indx) => (
-        <FormComponent
-          register={register}
-          form={form}
-          key={indx}
-          message={
-            form === "login" ? errors.login?.message : errors.password?.message
-          }
-        />
-      ))}
+	return (
+		<LoginWraper as="form" onSubmit={onSubmit}>
+			{login &&
+				(login === user.login ? (
+					password === user.password ? (
+						navigate("/entered")
+					) : (
+						<WarningComponent text={"Неверный пароль"} />
+					)
+				) : (
+					<WarningComponent text={`Пользователя ${login} не существует`} />
+				))}
+			{forms.map((form, indx) => (
+				<FormComponent
+					register={register}
+					form={form}
+					key={indx}
+					message={form === "login" ? errors.login?.message : errors.password?.message}
+				/>
+			))}
 
-      <LoginLabel>
-        <LoginCheckbox />
-        <LoginLabelText>Запомнить пароль</LoginLabelText>
-      </LoginLabel>
+			<LoginLabel>
+				<LoginCheckbox />
+				<LoginLabelText>Запомнить пароль</LoginLabelText>
+			</LoginLabel>
 
-      <LoginButton value="Войти" active={isBtnActive} disabled={!isBtnActive} />
-    </LoginWraper>
-  );
+			<LoginButton value="Войти" active={isBtnActive} disabled={!isBtnActive} />
+		</LoginWraper>
+	);
 };
 
 export default LoginPage;
